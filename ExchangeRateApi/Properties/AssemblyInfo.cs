7793c2c667f4dafe931654cfbe0aa3706ca49f37
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-07-30 15:26:29 +0300

    Provide an assembly information

--- ExchangeRateApi/Properties/AssemblyInfo.cs
+++ ExchangeRateApi/Properties/AssemblyInfo.cs
@@ -1,35 +1,23 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ExchangeRateApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Telegram bot that provide an exchange rate information for selected date")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExchangeRateApi")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCompany("Oleksand Boichuk")]
+[assembly: AssemblyProduct("ExchangeRateBot")]
+[assembly: AssemblyCopyright("© 2020 Oleksandr Boichuk")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("47FD995A-B5A6-452A-8982-D253406695B8")]
 
 // Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
+
+// MAJOR is a major release (usually many new features or changes to the UI or underlying OS)
+// MINOR is a minor release (perhaps some new features) on a previous major release
+// REVISION is usually a fix for a previous minor release (no new functionality)
+// BUILD NUMBER is incremented for each latest build of a revision.
+
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
