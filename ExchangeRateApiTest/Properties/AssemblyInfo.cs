4499680de063a52a7551a65089fb644bc2d0c3b3
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-07-30 15:32:33 +0300

    Add a project for unit tests

---
+++ ExchangeRateApiTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ExchangeRateApiTest")]
+[assembly: AssemblyDescription("Unit tests for Exchange Rate Api")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Oleksand Boichuk")]
+[assembly: AssemblyProduct("ExchangeRateBot")]
+[assembly: AssemblyCopyright("© 2020 Oleksandr Boichuk")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E9175B59-175E-4872-8DA3-048AF22A8481")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
