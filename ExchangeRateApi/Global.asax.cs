ac1e3571e50eb8151c4a4a24aa40fe3992773fa6
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-07-31 15:53:11 +0300

    Remove unnecessary references. Add NInject references

--- ExchangeRateApi/Global.asax.cs
+++ ExchangeRateApi/Global.asax.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace ExchangeRateApi
@@ -13,7 +12,6 @@ namespace ExchangeRateApi
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
