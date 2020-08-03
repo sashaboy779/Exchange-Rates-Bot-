cbb59f920b845b1c43147524e5ec822e1c3841d2
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-08-03 11:09:48 +0300

    Add command models. Add handlers models

---
+++ ExchangeRateApi/Models/TelegramBot/Commands/Command.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ExchangeRateApi.Models.TelegramBot.Commands
+{
+    public abstract class Command
+    {
+        public abstract string Identifier { get; }
+
+        public abstract Task Execute(Message message, TelegramBotClient client);
+    }
+}
