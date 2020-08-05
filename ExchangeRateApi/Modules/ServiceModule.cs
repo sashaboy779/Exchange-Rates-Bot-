ae2bc63675d2cd501c52663fda50f1dbb64824a3
Oleksandr Boichuk <sashaboychuk779@gmail.com>
2020-08-05 22:19:55 +0300

    Bind all services in NInject

---
+++ ExchangeRateApi/Modules/ServiceModule.cs
@@ -0,0 +1,19 @@
+﻿using ExchangeRateApi.Services;
+using ExchangeRateApi.Services.Interfaces;
+using Ninject.Modules;
+using Ninject.Web.Common;
+
+namespace ExchangeRateApi.Modules
+{
+    public class ServiceModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Kernel.Bind<IApiService>().To<ApiService>().InRequestScope();
+            Kernel.Bind<IUserService>().To<UserService>().InRequestScope();
+            Kernel.Bind<ICurrenciesService>().To<CurrenciesService>().InRequestScope();
+            Kernel.Bind<IExchangeRateService>().To<ExchangeRateService>().InRequestScope();
+            Kernel.Bind<ILocalizationService>().To<LocalizationService>().InRequestScope();
+        }
+    }
+}
